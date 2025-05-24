68304d84fe3b482ca57444f6d3236995fd3d6a4f
julietasegovia <julietavsegovia@gmail.com>
2025-05-24 12:40:03 -0300

    Add files via upload

--- estrategia.c
+++ estrategia.c
@@ -1,5 +1,6 @@
 #include "estrategia.h"
 #include "turno.h"
+#include "simulador.h"
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -54,9 +55,30 @@ void disponer(Nivel* nivel, Mapa* mapa) {
     }
 }
 
+Pila * pila_crear (int tamIn){
 
+    Pila *pilita = malloc(sizeof(Pila));
+    pilita->datos = malloc(sizeof(int) * tamIn);
+    pilita->tam = tamIn; 
+    
+    return  pilita;
+}
 
-Coordenada* posiciones_validas_backtracking(Coordenada *pos_validas, TipoCasilla **casillas, int alto, int ancho) {
+void pila_apilar(Pila *pilita, Coordenada nuevo) {
+    if(pilita->tam == pilita->ultimo) {
+        pilita->tam *= 2;
+        pilita->datos = realloc(pilita->datos, pilita->tam * sizeof(int));
+    }
+    pilita->datos[pilita->ultimo] = nuevo;
+    pilita->ultimo ++;
+}
+
+void pila_desapilar(Pila *pilita) {
+    pilita->ultimo --;
+}
+
+Coordenada* filtrar_validas(TipoCasilla **casillas, int alto, int ancho) {
+    Coordenada *validas = malloc (sizeof (Coordenada)*(alto * ancho));
     int cant_posiciones_validas = 0;
     
     for (int i = 0; i < alto; i++) {
@@ -68,140 +90,80 @@ Coordenada* posiciones_validas_backtracking(Coordenada *pos_validas, TipoCasilla
         }
     }
 
-    return pos_validas;
+    return validas;
 }
-//static int determinar_posicion_torre_backtracking(int *casilla_elegida, Coordenada *validas, int cant_validas) {
-  //  for(int i = 0; i < cant_validas; i++){
-    //    posibles_torres->torres[i] = validas[i];
-    //}
-//}
 
-int backtrack(Mapa* mapa, Nivel* nivel, Coordenada* pos_validas, int cant_validas, Coordenada* torres_actuales, int indice_torre) {
-    if (indice_torre == mapa->cant_torres) {
-        // Ya colocamos todas las torres, ahora evaluamos si la configuración es efectiva
-        int hubo_escape = simular_turno(mapa, nivel, torres_actuales, mapa->cant_torres);
-        return (hubo_escape == 0);
-    }
+Tower custom (Coordenada torre,  Mapa* mapita) {
+    Tower torre_ord;
+    torre_ord.ataque = 0;
+    int rango = mapita->distancia_ataque;
 
-    for (int i = 0; i < cant_validas; i++) {
-        // Evitar usar la misma casilla más de una vez
-        bool ya_usada = 0;
-        for (int j = 0; j < indice_torre; j++) {
-            if (torres_actuales[j].x == pos_validas[i].x &&
-                torres_actuales[j].y == pos_validas[i].y) {
-                ya_usada = 1;
-                break;
+    for (int i = -rango; i <= rango; i++)
+        for (int j = -rango; j <= rango; j++) {
+            
+            int hit_x = torre.x + i;
+            int hit_y = torre.y + j;
+
+            if (i == 0 && j == 0) continue;
+
+            if (hit_x >= 0 && hit_x < mapita->ancho && hit_y >= 0 && hit_y < mapita->alto)
+                if (mapita->casillas[hit_x][hit_y] == CAMINO) {
+                    torre_ord.ataque++;
             }
-        }
-        if (ya_usada) continue;
-
-        torres_actuales[indice_torre] = posiciones_validas[i];
-        
-        return (backtrack(mapa, nivel, posiciones_validas, cant_validas, torres_actuales, indice_torre + 1));
-
     }
+
+    torre_ord.pos_torre.x = torre.x;
+    torre_ord.pos_torre.y = torre.y;
+
+    return torre_ord;
 }
-void disponer_con_backtracking(Nivel* nivel, Mapa* mapa) {
-    int cantidad_casillas = mapa->alto * mapa->ancho;
-    Coordenada pos_validas[cantidad_casillas];
+
+int distancia_al_origen(Tower *torre) {
+    return torre->pos_torre.x * torre->pos_torre.x + torre->pos_torre.y * torre->pos_torre.y;
+}
+
+int comparar_torres(Tower *torre_a, Tower *torre_b) {
+    if (torre_b->ataque != torre_a->ataque) {
+        return torre_b->ataque - torre_a->ataque;
+    }
+
+    return distancia_al_origen(torre_a) - distancia_al_origen(torre_b);
+}
+
+void disponer_con_backtracking(Nivel* nivel, Mapa* mapa){
+    Coordenada *pos_validas = filtrar_validas(mapa->casillas, mapa->alto, mapa->ancho);
     int cant_validas = posiciones_validas(pos_validas, mapa->casillas, mapa->alto, mapa->ancho);
+    Pila *slots_torres = pila_crear(mapa->cant_torres);
 
-    Coordenada torres_actuales[mapa->cant_torres];
+    qsort(pos_validas, cant_validas, sizeof(Pila), distancia_al_origen); //ordeno para obtener la combinacion mas rapida
 
-    if (backtrack(mapa, nivel, pos_validas, cant_validas, torres_actuales, 0)) {
-        for (int i = 0; i < mapa->cant_torres; i++) {
-            colocar_torre(mapa, torres_actuales[i].x, torres_actuales[i].y, i);
-        }
+    for(int i=0; i<mapa->cant_torres; i++)
+        pila_apilar(slots_torres, pos_validas[i]); //apilo la primera combinacion
+        
+    for(int i = (mapa->cant_torres -1); i < cant_validas; i++){
+        for(int j = 0; j < mapa->cant_torres; j++)
+            if(simular_nivel(nivel, mapa, disponer_con_backtracking))    
+                colocar_torre(mapa, slots_torres->datos[j].x, slots_torres->datos[j].y, j);
+            else{
+                pila_desapilar(slots_torres);
+            }
     }
-}
 
-/*
-void disponer_con_backtracking(Nivel* nivel, Mapa* mapa) {
-    int cantidad_casillas = mapa->alto * mapa->ancho;
-    Coordenada casillas[cantidad_casillas];
-    int casilla_elegida[cantidad_casillas];
-    int cant_validas = posiciones_validas(casillas, mapa->casillas, mapa->alto, mapa->ancho);
-    Coordenada validas = posiciones_validas_backtracking(*casilla_elegida, validas, cant_validas);
-    for(int i = 0; i < cantidad_casillas; casilla_elegida[i++] = 0);
-    
-    Pila posibles_torres[mapa->cant_torres];
-    posibles_torres->ultimo = 0;
 
-    for(int torre = 0; torre < mapa->cant_torres; torre++){
-        int nueva_torre = determinar_posicion_torre(casilla_elegida, cant_validas);
-        casilla_elegida[nueva_torre] = 1;
-        posibles_torres->torres[torre].x = validas[nueva_torre].x;
-        posibles_torres->torres[torre].y = validas[nueva_torre].y;
-        posibles_torres->ultimo++;
-    }
-        if(simular_turno(mapa, nivel, posibles_torres->torres, ))
-            for(int torre_a_colocar =0; torre_a_colocar < mapa->cant_torres; torre_a_colocar++)
-            colocar_torre(mapa, posibles_torres->torres[torre_a_colocar].x, posibles_torres->torres[torre_a_colocar].y, torre_a_colocar);
-        else
-            pila_desapilar(posibles_torres);
     return;
 }
-*/
 
-//la idea es que se coloquen las torres lo mas arriba y con mayor ataque posible (en esquinas por ej). Pense en hacer un orden de prioridad, alcance y cercania al inicio
-/* typedef struct {
-    int ancho, alto, cant_torres, distancia_ataque;
-    TipoCasilla **casillas;
-    Coordenada *torres;
-} Mapa;
-*/
-
-CasoMapa bifurcacion(Mapa *mapita){
-    CasoMapa bifurcacion;
-    bifurcacion.hay_bifurcacion = 0;
-
-    for (int i = 1; i < mapita->ancho - 1; i++)
-        for (int j = 1; j < mapita->alto - 1; j++) {
-            if (mapita->casillas[i][j] == CAMINO) {
-                int vecinos = 0;
-                if (mapita->casillas[i+1][j] == CAMINO) vecinos++;
-                if (mapita->casillas[i-1][j] == CAMINO) vecinos++;
-                if (mapita->casillas[i][j+1] == CAMINO) vecinos++;
-                if (mapita->casillas[i][j-1] == CAMINO) vecinos++;
-
-                if(vecinos >=3){
-                    bifurcacion.hay_bifurcacion = 1;
-                    bifurcacion.pos_bifurcacion.x = i;
-                    bifurcacion.pos_bifurcacion.y = j;
-                    return bifurcacion;
-                }
-            }
-        }
-
-    return bifurcacion;
-}
-
-int custom (Coordenada torre,  Mapa* mapa) {
-    int ataque = 0;
-    int alcance = mapa->distancia_ataque;
-    for (int i = torre->x - alcance; i <= torre->x + alcance; i++) {
-        for (int j = torre->y - alcance; j <= torre->y + alcance; j++) {
-            if (i >= 0 && i < mapa->alto && j >= 0 && j < mapa->largo) {
-                if (mapa->casillas[i][j] == CAMINO) { 
-                    ataque++;
-                }
-            }
-        }
-    }
-    return ataque;
-}
-
-//no esta hecho!!
 void disponer_custom(Nivel* nivel, Mapa* mapa) {
-    int cantidad_casillas = mapa->alto * mapa->ancho;
-    Coordenada pos_validas[cantidad_casillas];
+
+    Coordenada *pos_validas = filtrar_validas(mapa->casillas, mapa->alto, mapa->ancho);
     int cant_validas = posiciones_validas(pos_validas, mapa->casillas, mapa->alto, mapa->ancho);
+    Tower *posibles_torres = malloc(sizeof(Tower) * cant_validas);
 
-    Coordenada torres_actuales[mapa->cant_torres];
-
-    if () {
-        for (int i = 0; i < mapa->cant_torres; i++) {
-            colocar_torre(mapa, torres_actuales[i].x, torres_actuales[i].y, i);
-        }
-    }
+    for(int i = 0; i<cant_validas; i++)
+    posibles_torres[i] = custom(pos_validas[i], mapa); //lleno el arreglo
+    
+    qsort(posibles_torres, cant_validas, sizeof(Tower), comparar_torres); //ordena por primera prioridad el ataque y por segunda la cerania a (0,0)
+    
+    for(int i = 0; i < mapa->cant_torres; i++)
+        colocar_torre(mapa, posibles_torres[i].pos_torre.x, posibles_torres[i].pos_torre.y, i); //coloca una i cantidad de torres
 }
--- estrategia.h
+++ estrategia.h
@@ -4,20 +4,26 @@
 #include "mapa.h"
 #include "nivel.h"
 
-typedef struct Stack_{
-    Coordenada *torres;
-    int ultimo;
-}Pila;
+typedef struct Stack_ {
+int tam;
+Coordenada *datos;
+int ultimo;
+} Pila;
+
 
 typedef struct Queque_{
     Coordenada *vacios;
     int ultimo, primero;
 }Cola;
 
-typedef struct Caso_{
-    int hay_bifurcacion;    //0 si no hay bifurcaciones, 1 si las hay
-    Coordenada pos_bifurcacion;     //irrelevante si hay_bifurcacion = 0
-}CasoMapa;
+typedef struct Torre_{
+    Coordenada pos_torre;
+    int ataque;
+}Tower;
+
+int comparar_torres(Tower *a, Tower *b);
+
+int distancia_al_origen(Tower *torre);
 
 typedef void (*DisposicionTorres)(Nivel*, Mapa*);
 
@@ -25,9 +31,7 @@ void disponer(Nivel* nivel, Mapa* mapa);
 
 void disponer_con_backtracking(Nivel* nivel, Mapa* mapa);
 
-Coordenada* posiciones_validas_backtracking(Coordenada *validas, TipoCasilla **casillas, int alto, int ancho);
-
-static int determinar_posicion_torre_backtracking(int *casilla_elegida, Coordenada *validas);
+Coordenada* posiciones_validas_backtracking(TipoCasilla **casillas, int alto, int ancho);
 
 void disponer_custom(Nivel* nivel, Mapa* mapa);
 
